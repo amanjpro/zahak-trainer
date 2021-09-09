239ebf085ad4e7f3e0b73eddacb54b6d2c3c7835
amanjpro <amanjpro@gmail.com>
2021-09-08 23:25:13 -0400

    Add a very simple command line parsing

--- main.go
+++ main.go
@@ -1,8 +1,39 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+	DefaultNumberOfEpochs        = 100
+	DefaultNumberOfInputs        = 100
+	DefaultNumberOfHiddenLayers  = 100
+	DefaultNumberOfHiddenNeurons = 100
+	DefaultNumberOfOutputs       = 100
+)
+
 func main() {
-	topology := NewTopology(14, 13, 12, 11)
-	net := CreateNetwork(topology)
-	net.SaveCheckpoint("/tmp/net")
-	net = LoadCheckpoint("/tmp/net")
+	epochs := flag.Int("epochs", DefaultNumberOfEpochs, "Number of epochs")
+	inputs := flag.Int("inputs", DefaultNumberOfInputs, "Number of inputs")
+	layers := flag.Int("layers", DefaultNumberOfHiddenLayers, "Number of hidden layers")
+	neurons := flag.Int("neurons", DefaultNumberOfHiddenNeurons, "Number of hidden neurons")
+	outputs := flag.Int("outputs", DefaultNumberOfOutputs, "Number of outputs")
+
+	checkpointPath := flag.String("from-checkpoint", "", "Start from a known checkpoint")
+	saveCheckpointPath := flag.String("to-checkpoint", "", "Save checkpoints to")
+	binPath := flag.String("nnue-path", "", "Final NNUE path")
+	epdPath := flag.String("input-path", "", "Path to input dataset")
+
+	flag.Parse()
+
+	topology := NewTopology(*inputs, *outputs, *layers, *neurons)
+	network := CreateNetwork(topology)
+
+	if *checkpointPath != "" {
+		network = LoadCheckpoint(*checkpointPath)
+	}
+
+	fmt.Println("Options:", *epochs, *saveCheckpointPath, *binPath, *epdPath)
+	network.SaveCheckpoint(*saveCheckpointPath)
 }
